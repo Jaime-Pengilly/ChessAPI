5dc8c5308f51fa74ea0dbb98b391c4e784ba7123
Jaime-Pengilly <116103802+Jaime-Pengilly@users.noreply.github.com>
2024-04-15 09:21:54 -0500

    Initial Commit

---
+++ CoolChessAPI/CoolChessAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolChessAPIApp.swift
+//  CoolChessAPI
+//
+//  Created by PENGILLY, JAIME on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct CoolChessAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
