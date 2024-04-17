1b32ab57b6b7e9415b53c8c2bf0b0510e67c2531
Jaime-Pengilly <116103802+Jaime-Pengilly@users.noreply.github.com>
2024-04-17 09:05:30 -0500

    aggggg

--- CoolChessAPI/ContentView.swift
+++ CoolChessAPI/ContentView.swift
@@ -7,18 +7,88 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+struct User: Codable, Hashable {
+    public var username: String
+    public var country: String
+    public var league: String
+    public var url: String
+//    public var chess_rapid: String
+    public var avatar: String
 }
 
+struct Result: Codable {
+    var items: [User]
+}
+
+struct ContentView: View {
+    @State var users:[User] = []
+    @State var searchText = ""
+    
+    var body: some View {
+        NavigationStack{
+            if users.count == 0 && !searchText.isEmpty{
+                VStack {
+                    ProgressView().padding()
+                    Text("Fetching Users...")
+                        .foregroundStyle(Color.pink)
+                        .onAppear{
+                            getUsers()
+                        }
+                }
+            } else {
+                List(users, id:\.self) {user in
+                    Link(destination:URL(string:user.url)!){
+                        
+                        HStack(alignment:.top){
+                            AsyncImage(url:URL(string: user.avatar)){ response
+                                in
+                                switch response {
+                                case .success(let image):
+                                    image.resizable()
+                                        .frame(width:50, height: 50)
+                                default:
+                                    Image(systemName:"nosign")
+                                }
+                            }
+                        }
+                        VStack(alignment: .leading) {
+                            Text(user.username)
+                            Text("\(user.url)")
+                                .font(.system(size:11))
+                                .foregroundColor(Color.gray)
+                        }
+                    }
+                }
+                }
+        }.searchable(text:$searchText)
+            .onSubmit(of: .search){
+                getUsers()
+        }
+    }
+    
+    
+    func getUsers(){
+        let trimmedSearchText = searchText.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard !trimmedSearchText.isEmpty else {
+            return
+        }
+        if let apiURL =
+            URL(string: "https://api.chess.com/pub/player/hikaru"){
+            var request = URLRequest(url:apiURL)
+            request.httpMethod = "GET"
+            URLSession.shared.dataTask(with: request){
+                data, response,error in
+                if let userData = data {
+                    if let usersFromAPI = try? JSONDecoder().decode(Result.self, from: userData){
+                        users = usersFromAPI.items
+                        print(users)
+                    }
+                }
+            }.resume()
+        }
+    }
+}
 #Preview {
     ContentView()
 }
